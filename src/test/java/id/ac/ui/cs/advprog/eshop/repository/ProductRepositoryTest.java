00a907d73e40d470c7d9b0a1575cd8a16d191791
bryanjeshua <bryanjmt04@gmail.com>
2024-02-06 23:53:50 +0700

    pushing to unit-test branch since i skipped

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/ProductRepositoryTest.java
@@ -46,7 +46,6 @@ class ProductRepositoryTest {
 
     @Test
     void testFindAllIfMoreThanOneProduct() {
-
         Product product1 = new Product();
         product1.setProductId("eb558e9f-1c39-460e-8860-71af6af63bd6");
         product1.setProductName("Sampo Cap Bambang");
