9249183fb9d06dcfa7108e409dff7c47c133c68b
bryanjeshua <bryanjmt04@gmail.com>
2024-03-05 16:57:41 +0700

    [GREEN] Implement OrderRepository class.

--- src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/repository/OrderRepositoryTest.java
@@ -81,14 +81,14 @@ class OrderRepositoryTest {
     void testFindByIdIfIdFound() {
         for (Order order : orders) {
             orderRepository.save(order);
-    
-            Order findResult = orderRepository.findById(orders.get(1).getId());
-    
-            assertEquals(orders.get(1).getId(), findResult.getId());
-            assertEquals(orders.get(1).getOrderTime(), findResult.getOrderTime());
-            assertEquals(orders.get(1).getAuthor(), findResult.getAuthor());
-            assertEquals(orders.get(1).getStatus(), findResult.getStatus());
         }
+        Order findResult = orderRepository.findById(orders.get(1).getId());
+
+        assertEquals(orders.get(1).getId(), findResult.getId());
+        assertEquals(orders.get(1).getOrderTime(), findResult.getOrderTime());
+        assertEquals(orders.get(1).getAuthor(), findResult.getAuthor());
+        assertEquals(orders.get(1).getStatus(), findResult.getStatus());
+
     }
     @Test
     void testFindByIdIfIdNotFound() {
